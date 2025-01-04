6f78f6b94e3f4f04ca21b8ce563deaca8bd67b2b
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-01-04 20:03:12 +0100

    Add files via upload

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -4,14 +4,12 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `ღ ${lenguajeGB['smsAddB5']()} ${pesan}`
-let teks = `╭━〔 *${lenguajeGB['smstagaa']()}* 〕━⬣\n\n${oi}\n\n`
+let oi = `❄️𝔐𝔦𝔠𝔥𝔦𝔅𝔬𝔱 𝔱𝔢 𝔡𝔢𝔰𝔢𝔞 𝔲𝔫𝔞 𝔣𝔢𝔩𝔦𝔷 𝔫𝔞𝔳𝔦𝔡𝔞𝔡!❄️ ${pesan}`
+let teks = `*𝘼𝙘𝙩𝙞𝙫𝙚𝙣𝙨𝙚 𝙗𝙤𝙩𝙨 𝙃𝙤𝙃𝙤𝙃𝙤🎅🏻*\n${oi}\n\n*💅🏻@𝔠𝔢𝔬𝔤𝔢𝔯𝔦𝔭𝔦𝔲𝔪.𝔡𝔷𝔫*\n`
 for (let mem of participants) {
-teks += `┃⊹ @${mem.id.split('@')[0]}\n`}
-teks += `┃\n`
-teks += `┃ ${wm}\n`
-teks += `╰━━━━━[ *𓃠 ${vs}* ]━━━━━⬣`
-conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
+teks += `☃️🎅🏻ᝰ.ᐟ @${mem.id.split('@')[0]}\n`}
+teks += `${wm}`
+conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) } )
 }
 handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
 handler.admin = true
--- plugins/jadibot-lista.js
+++ plugins/jadibot-lista.js
@@ -1,42 +1,49 @@
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import ws from 'ws'
+import ws from 'ws';
+async function handler(m, { conn: _envio, usedPrefix }) {
+  const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
+  function convertirMsADiasHorasMinutosSegundos(ms) {
+  var segundos = Math.floor(ms / 1000);
+  var minutos = Math.floor(segundos / 60);
+  var horas = Math.floor(minutos / 60);
+  var días = Math.floor(horas / 24);
 
-async function handler(m, { conn, usedPrefix, command }) {
-// carpetas creadas
-const __filename = fileURLToPath(import.meta?.url)
-const __dirname = path?.dirname(__filename)
-const carpetaBase = path?.resolve(__dirname, '..', 'GataJadiBot')
-const cantidadCarpetas = (fs?.readdirSync(carpetaBase, { withFileTypes: true }).filter(item => item?.isDirectory())?.length) || 0
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
 
-// servidor
-let _uptime = process.uptime() * 1000
-let uptime = convertirMs(_uptime)
-  
-const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])]
+  var resultado = "";
+  if (días !== 0) {
+    resultado += días + " días, ";
+  }
+  if (horas !== 0) {
+    resultado += horas + " horas, ";
+  }
+  if (minutos !== 0) {
+    resultado += minutos + " minutos, ";
+  }
+  if (segundos !== 0) {
+    resultado += segundos + " segundos";
+  }
 
-const message = users.map((v, index) => `👤 \`[${index + 1}]\` *${v.user.name || global.db.data.users[v.user.jid]?.name || 'Anónimo' }*
-⏱️ \`\`\`${v.uptime ? convertirMs(Date.now() - v.uptime) : "Desconocido"}\`\`\`
-🐈 wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}serbot%20code`).join('\n\n∵ ∵ ∵ ∵ ∵ ∵ ∵ ∵ ∵ ∵\n\n')
-const replyMessage = message.length === 0 ? `*NO HAY SUB BOTS DISPONIBLE. VERIFIQUE MÁS TARDE.*\n🐈 wa.me/${conn.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}serbot%20code` : message
-const totalUsers = users.length
-const responseMessage = `☄️ *LISTA DE SUB-BOTS V${vsJB}*\n
-\`¡Conviértete en sub bot desde otros sub bots!\`\n
-🔄 *Auto conexión automática*
-✨ *Novedades:* 
-_${canal1}_
-
-${totalUsers ? `💠 *Sub Bots conectados:* ${totalUsers || 0}\n` : ''}${cantidadCarpetas ? `📁 *Sesiones creadas:* ${cantidadCarpetas}\n` : ''}${totalUsers ? `📁 *Sesiones activas:* ${totalUsers || 0}\n` : ''}💻 *Servidor:* \`\`\`${uptime}\`\`\`\n\n${replyMessage.trim()}`.trim()
-await conn.sendMessage(m.chat, { image: { url: ['https://qu.ax/spUwF.jpeg', 'https://qu.ax/ZfKAD.jpeg', 'https://qu.ax/UKUqX.jpeg'].getRandom() }, caption: responseMessage }, { quoted: m })
+  return resultado;
 }
-handler.command = /^(listjadibots|bots|subsbots)$/i
-export default handler
 
-function convertirMs(ms) {
-const s = Math.floor(ms / 1000) % 60;
-const m = Math.floor(ms / 60000) % 60;
-const h = Math.floor(ms / 3600000) % 24;
-const d = Math.floor(ms / 86400000);
-return [ d > 0 ? `${d}d` : "", `${h}h`, `${m}m`, `${s}s` ].filter(Boolean).join(" ")
+  const message = users.map((v, index) => `*${index + 1}.-* @${v.user.jid.replace(/[^0-9]/g, '')}\n*Link:* wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}estado\n*Nombre:* ${v.user.name || '-'}\n*Uptime:* ${ v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : "Desconocido"}`).join('\n\n');
+  const replyMessage = message.length === 0 ? '*—◉ No hay SubBots activos en estos momentos.*' : message;
+  const totalUsers = users.length;
+  const responseMessage = `
+*—◉ 𝙰𝚀𝚄𝙸 𝚃𝙸𝙴𝙽𝙴𝚂 𝙻𝙰 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝚂𝚄𝙱𝙱𝙾𝚃𝚂 (𝚂𝙴𝚁𝙱𝙾𝚃/𝙹𝙰𝙳𝙸𝙱𝙾𝚃) 𝙰𝙲𝚃𝙸𝚅𝙾𝚂 🤖️*
+
+*◉ 𝙿𝚄𝙴𝙳𝙴𝚂 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙰𝚁𝙻𝙾𝚂 𝙿𝙰𝚁𝙰 𝙿𝙴𝙳𝙸𝚁 𝚀𝚄𝙴 𝚂𝙴 𝚄𝙽𝙰𝙽 𝙰 𝚃𝚄 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙴 𝚁𝙴𝚂𝙿𝙴𝚃𝚄𝙾𝚂𝙾!!*
+
+*[❗] 𝚃𝙷𝙴 𝚂𝙷𝙰𝙳𝙾𝚆 𝙱𝚁𝙾𝙺𝙴𝚁𝚂 - 𝚃𝙴𝙰𝙼 𝚂𝙴 𝙳𝙴𝚂𝙻𝙸𝙽𝙳𝙰 𝙳𝙴 𝚃𝙾𝙳𝙰 𝚁𝙴𝚂𝙿𝙾𝙽𝚂𝙰𝙱𝙸𝙻𝙸𝙳𝙰𝙳 𝙾 𝚂𝚄𝙲𝙴𝚂𝙾 𝙾𝙲𝚄𝚁𝚁𝙸𝙳𝙾 𝙲𝙾𝙽 𝚁𝙴𝚂𝙿𝙴𝙲𝚃𝙾 𝙰𝙻 𝙱𝙾𝚃 𝙾 𝚂𝚄𝙱𝙱𝙾𝚃𝚂*
+
+*—◉ 𝚂𝚄𝙱𝙱𝙾𝚃𝚂 𝙲𝙾𝙽𝙴𝙲𝚃𝙰𝙳𝙾𝚂:* ${totalUsers || '0'}
+
+${replyMessage.trim()}`.trim();
+
+  await _envio.sendMessage(m.chat, {text: responseMessage, mentions: _envio.parseMention(responseMessage)}, {quoted: m});
 }
+handler.command = handler.help = ['listjadibot', 'bots', 'subsbots'];
+handler.tags = ['jadibot'];
+export default handler;
