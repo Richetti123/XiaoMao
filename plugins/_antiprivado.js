943d3b724b99ba88b67632a9149341b07cb81fb3
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-01-05 16:19:09 +0100

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,37 +1,13 @@
-const comandos = /piedra|papel|tijera|estado|verificar|code|jadibot --code|--code|creadora|bottemporal|grupos|instalarbot|términos|bots|deletebot|eliminarsesion|serbot|verify|register|registrar|reg|reg1|nombre|name|nombre2|name2|edad|age|edad2|age2|genero|género|gender|identidad|pasatiempo|hobby|identify|finalizar|pas2|pas3|pas4|pas5|registroc|deletesesion|registror|jadibot/i
-
-export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
-let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
-
-let setting = global.db.data.settings[this.user.jid]
-const settingsREAD = global.db.data.settings[this.user.jid] || {}
-
-if (m.text && prefixRegex.test(m.text)) {
-//await conn.sendPresenceUpdate('composing', m.chat)
-//await conn.readMessages([m.key])
-        
-let usedPrefix = m.text.match(prefixRegex)[0]
-let command = m.text.slice(usedPrefix.length).trim().split(' ')[0]
-}
-
-if (m.fromMe) return !0
-if (m.isGroup) return !1
-if (!m.message) return !0
-//if (m.chat === "120363336642332098@newsletter") return; 
-const regexWithPrefix = new RegExp(`^${prefix.source}\\s?${comandos.source}`, 'i')
-if (regexWithPrefix.test(m.text.toLowerCase().trim())) return !0
-
-let chat, user, bot, mensaje
-chat = global.db.data.chats[m.chat]
-user = global.db.data.users[m.sender]
-bot = global.db.data.settings[this.user.jid] || {}
-
-if (bot.antiPrivate && !isOwner && !isROwner) {
-await conn.sendPresenceUpdate('composing', m.chat)
-await conn.readMessages([m.key])
-await conn.reply(m.chat, mid.mAdvertencia + mid.smsprivado(m, cuentas), m, { mentions: [m.sender] })  
-await conn.updateBlockStatus(m.chat, 'unblock')
-//await this.updateBlockStatus(m.sender, 'block')
-}
-return !1
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+  if (m.isBaileys && m.fromMe) return !0;
+  if (m.isGroup) return !1;
+  if (!m.message) return !0;
+  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
+  const chat = global.db.data.chats[m.chat];
+  const bot = global.db.data.settings[this.user.jid] || {};
+  if (bot.antiPrivate && !isOwner && !isROwner) {
+    await m.reply(`*@${m.sender.split`@`[0]} Esta prohibido escribir al privado*\n\n> *Si quieres comprar el bot o tienes alguna consulta solo responde a este mensaje el bot te atenderá`, false, {mentions: [m.sender]});
+    await this.updateBlockStatus(m.chat, 'unblock');
+  }
+  return !1;
 }
