578ef1bce8d30e47cc5b5e135242ad515d66339f
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-01-05 16:27:13 +0100

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -4,10 +4,33 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   if (!m.message) return !0;
   if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
   const chat = global.db.data.chats[m.chat];
+  const user = global.db.data.users[m.sender];
   const bot = global.db.data.settings[this.user.jid] || {};
-  if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`*@${m.sender.split`@`[0]} Esta prohibido escribir al privado*\n\n> *Si quieres comprar el bot o tienes alguna consulta solo responde a este mensaje el bot te atenderá`, false, {mentions: [m.sender]});
-    await this.updateBlockStatus(m.chat, 'unblock');
-  }
-  return !1;
+//INICIO lineas por Kurt18
+const fechaActual = new Date();
+const fechaActualNum = Date.UTC(
+    fechaActual.getFullYear(),
+    fechaActual.getMonth(),
+    fechaActual.getDate()
+);
+let q = m.quoted ? m.quoted : m;
+let mime = (q.msg || q).mimetype || q.mediaType || "";
+
+let horaActualNum = Date.now();
+const intervaloEsperado = 12 * 60 * 60 * 1000; // 1 hora en milisegundos
+let tiempoTranscurrido = horaActualNum - user.fechaUltimoMsjInbox;
+
+console.log(`user.fechaUltimoMsjInbox >${user.fechaUltimoMsjInbox}<`); //Luego comentar
+console.log(`horaActualNum >${horaActualNum}<`); //Luego comentar
+console.log(`tiempoTranscurrido >${tiempoTranscurrido}<`); //Luego comentar
+
+if (tiempoTranscurrido >= intervaloEsperado) {
+   await m.reply(`*🤖Hola @${m.sender.split`@`[0]}, esta prohibido utilizar el bot en privado😼*\n\n> Pero si quieres comprarlo o tienes alguna otra consulta solo responde a este mensaje y el bot te atendera\n\n🤖${wm}🤖`, false, {mentions: [m.sender]});
+    user.fechaUltimoMsjInbox = horaActualNum;
+    return;
 }
+//user.fechaUltimoMsjInbox = 0; //test (luego eliminar)
+//FIN lineas por Kurt18
+
+  return !1;
+                  }
