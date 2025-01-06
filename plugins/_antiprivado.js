4b8bd39d59c01f3cd8f6a48121181d2d6a2924aa
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-01-06 03:01:03 +0100

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,36 +1,38 @@
-export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
-  if (m.isBaileys && m.fromMe) return !0;
-  if (m.isGroup) return !1;
-  if (!m.message) return !0;
-  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
-  const chat = global.db.data.chats[m.chat];
-  const user = global.db.data.users[m.sender];
-  const bot = global.db.data.settings[this.user.jid] || {};
-//INICIO lineas por Kurt18
-const fechaActual = new Date();
-const fechaActualNum = Date.UTC(
-    fechaActual.getFullYear(),
-    fechaActual.getMonth(),
-    fechaActual.getDate()
-);
-let q = m.quoted ? m.quoted : m;
-let mime = (q.msg || q).mimetype || q.mediaType || "";
+/*const comandos = /piedra|papel|tijera|estado|verificar|code|jadibot --code|--code|creadora|bottemporal|grupos|instalarbot|términos|bots|deletebot|eliminarsesion|serbot|verify|register|registrar|reg|reg1|nombre|name|nombre2|name2|edad|age|edad2|age2|genero|género|gender|identidad|pasatiempo|hobby|identify|finalizar|pas2|pas3|pas4|pas5|registroc|deletesesion|registror|jadibot/i
 
-let horaActualNum = Date.now();
-const intervaloEsperado = 12 * 60 * 60 * 1000; // 1 hora en milisegundos
-let tiempoTranscurrido = horaActualNum - user.fechaUltimoMsjInbox;
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
+let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
 
-console.log(`user.fechaUltimoMsjInbox >${user.fechaUltimoMsjInbox}<`); //Luego comentar
-console.log(`horaActualNum >${horaActualNum}<`); //Luego comentar
-console.log(`tiempoTranscurrido >${tiempoTranscurrido}<`); //Luego comentar
+let setting = global.db.data.settings[this.user.jid]
+const settingsREAD = global.db.data.settings[this.user.jid] || {}
 
-if (tiempoTranscurrido >= intervaloEsperado) {
-   await m.reply(`*🤖Hola @${m.sender.split`@`[0]}, esta prohibido utilizar el bot en privado😼*\n\n> Pero si quieres comprarlo o tienes alguna otra consulta solo responde a este mensaje y el bot te atendera\n\n🤖${wm}🤖`, false, {mentions: [m.sender]});
-    user.fechaUltimoMsjInbox = horaActualNum;
-    return;
+if (m.text && prefixRegex.test(m.text)) {
+//await conn.sendPresenceUpdate('composing', m.chat)
+//await conn.readMessages([m.key])
+        
+let usedPrefix = m.text.match(prefixRegex)[0]
+let command = m.text.slice(usedPrefix.length).trim().split(' ')[0]
 }
-//user.fechaUltimoMsjInbox = 0; //test (luego eliminar)
-//FIN lineas por Kurt18
 
-  return !1;
-                  }
+if (m.fromMe) return !0
+if (m.isGroup) return !1
+if (!m.message) return !0
+if (m.chat === "120363336642332098@newsletter") return; 
+const regexWithPrefix = new RegExp(`^${prefix.source}\\s?${comandos.source}`, 'i')
+if (regexWithPrefix.test(m.text.toLowerCase().trim())) return !0
+
+let chat, user, bot, mensaje
+chat = global.db.data.chats[m.chat]
+user = global.db.data.users[m.sender]
+bot = global.db.data.settings[this.user.jid] || {}
+
+if (bot.antiPrivate && !isOwner && !isROwner) {
+await conn.sendPresenceUpdate('composing', m.chat)
+await conn.readMessages([m.key])
+await conn.reply(m.chat, mid.mAdvertencia + mid.smsprivado(m, cuentas), m, { mentions: [m.sender] })  
+await conn.updateBlockStatus(m.chat, 'block')
+//await this.updateBlockStatus(m.sender, 'block')
+}
+return !1
+}
+*/
